0c4bf448b75720cdaf5003f426f6af6447561d5a
Andrea Biancini <andrea.biancini@gmail.com>
2020-01-05 11:10:09 +0100

    Refactored script

--- scripts/visit_profiles.js
+++ scripts/visit_profiles.js
@@ -2,14 +2,34 @@ function sleep(milliseconds) {
     return new Promise(r => setTimeout(r, milliseconds));
 }
 
+async function visitUser(name, href) {
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function() {
+        console.log("Visited user " + name + ": " + href);
+    };
+    oReq.open("get", href, true);
+    oReq.send();
+
+    await sleep(200);
+}
+
+async function moveToNextPage() {
+    var pagination = document.getElementsByTagName('artdeco-pagination')[0];
+    var button = pagination.getElementsByClassName('artdeco-pagination__button--next')[0];
+
+    if (!button || button.disabled) return false;
+    button.click();
+    
+    await sleep(1000);
+    return true;
+}
+
 async function run() {
     var process = true;
-    var i = 0;
 
     while (process) {
-        i = i + 1;
         window.scrollTo(0, document.body.scrollHeight);
-        await sleep(1000);
+        await sleep(500);
         
         var list = document.querySelector('.search-results__list');
         var nodes = list.getElementsByTagName('li');
@@ -20,24 +40,11 @@ async function run() {
             var link = parent.getElementsByClassName('search-result__result-link')[0];
             
             if (!link) continue;
-            var href = link.href;
             var span = link.getElementsByClassName('actor-name')[0];
-            var name = span.innerHTML;
-            
-            console.log(i + ") Utente " + name + ": " + href);
-            var oReq = new XMLHttpRequest();
-            oReq.onload = function() {
-                //console.log(this.responseText);
-            };
-            oReq.open("get", href, true);
-            oReq.send();
+            await visitUser(span.innerHTML, link.href);
         }
 
-        var pagination = document.getElementsByTagName('artdeco-pagination')[0];
-        var button = pagination.getElementsByClassName('artdeco-pagination__button--next')[0];
-        if (!button || button.disabled) process = false;
-        button.click();
-        await sleep(1500);
+        process = await moveToNextPage();
     }
 
     console.log("Script ended.");
